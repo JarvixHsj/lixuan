d8278d9ada5218b238d4bf4ffcb76e31563608c7
jarvixHsj <jarvix@sina.com>
2017-08-10 00:14:19 +0800

    发货 up

--- extend/model/User.php
+++ extend/model/User.php
@@ -9,7 +9,34 @@ class User extends Model
     public  function updatePassword($where = array(), $save = array())
     {
         return $this->allowField(true)->update($save,$where);
+    }
 
+    /**
+     * 获取用户信息
+     * @param $id
+     * @return array|bool
+     */
+    public function getUserInfo($id)
+    {
+        $res = $this->find($id);
+        if($res){
+            return $res->toArray();
+        }else{
+            return false;
+        }
+    }
+
+
+    /**
+     * 获取所有可用用户
+     */
+    public function getUserList()
+    {
+        $res = $this->where('status',1)->select();
+        if($res){
+            return $res->toArray();
+        }
+        return false;
     }
 
 }
